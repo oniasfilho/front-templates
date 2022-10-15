e2cb11f4e4df860fd3d5e0070580b7e00681cbbd
Onias da Rocha <onias.filho@gmail.com>
2022-10-14 23:32:41 -0400

    finishes login page

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-const rightArea = document.getElementById("page-wrapper")
+const pageWrapper = document.getElementById("page-wrapper") //tela pequena
+const rightArea = document.getElementById("right-area") // tela grande
+const body = document.querySelector("body")
+
 
 const changeBackgroundImage = () => {
-  let randomNumber = Math.floor(Math.random() * (4 - 1) + 1)
+  let randomNumber = Math.floor(Math.random() * (6 - 1) + 1)
 
-  // rightArea.style.backgroundImage = `linear-gradient(to right bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.186)), url("/static/bg${randomNumber}.jpg,  background-repeat: no-repeat")`;
-  rightArea.style.backgroundImage = `linear-gradient(to right bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.186)), url("/static/bg2-square.jpg" )`;
-  rightArea.style.backgroundSize = "cover"
-  // rightArea.style.backgroundPosition = "left"
-  // rightArea.style = "background-size: cover"
+  console.log(pageWrapper.style.backgroundImage)
+
+  let screenSize = window.innerWidth;
+
+  setBackgroundForSmallScreen = () => {
+    pageWrapper.style.backgroundImage = `linear-gradient(to right bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.186)), url("/static/bg${randomNumber}.jpg")`;
+    pageWrapper.style.backgroundSize = "cover"
+
+    rightArea.style.backgroundImage = ""
+    rightArea.style.backgroundSize = ""
+  }
+
+  setBackgroundForBigScreen = () => {
+    rightArea.style.backgroundImage = `linear-gradient(to right bottom, rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.186)), url("/static/bg${randomNumber}.jpg")`;
+    rightArea.style.backgroundSize = "cover"
+  }
+
+  if (screenSize > 999) {
+    setBackgroundForBigScreen();
+  } else {
+    setBackgroundForSmallScreen();
+  }
+
+  window.onresize = (e) => {
+    screenSize = e.target.innerWidth
+    if (screenSize > 999) {
+      setBackgroundForBigScreen();
+    } else {
+      setBackgroundForSmallScreen();
+    }
+  }
+
 }
 
 changeBackgroundImage();
